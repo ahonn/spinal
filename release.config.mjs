9c40276e97d6b6568921b5354149f20e82a7a480
ahonn <ahonn95@outlook.com>
2023-07-05 17:52:00 +0800

    ci: use yarn as package manager and add release workflow

---
+++ release.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    'semantic-release-yarn',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
+};
